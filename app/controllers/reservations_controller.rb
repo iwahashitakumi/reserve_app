5aea069a1ae07cea4c673ad8d22e4650037b0b6e
岩橋拓海 <minton@kin.local>
2023-08-10 23:10:29 +0900

    予約エラー時のエラー改善

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -13,7 +13,6 @@ class ReservationsController < ApplicationController
   def create
     @room = Room.find(params[:reservation][:room_id])
       @reservation = Reservation.new(params.require(:reservation).permit(:start_date, :end_date, :people, :total, :user_id, :room_id))
-      @reservation.total = @reservation.room.money * @reservation.people * (@reservation.end_date.to_date + 1.day - @reservation.start_date.to_date ).to_i
       if @reservation.save
         redirect_to reservations_path
       else
@@ -23,11 +22,10 @@ class ReservationsController < ApplicationController
 
   def confirm
     @room = Room.find(params[:reservation][:room_id])
-    @reservation = Reservation.new(params.require(:reservation).permit(:start_date, :end_date, :people, :total, :user_id, :room_id))
     if @room.invalid? 
       redirect_to :rooms 
     end
-    @reservation.total =  @reservation.room.money * @reservation.people * (@reservation.end_date.to_date - @reservation.start_date.to_date ).to_i
+    @reservation = Reservation.new(params.require(:reservation).permit(:start_date, :end_date, :people, :total, :user_id, :room_id))
   end
 
   end
